8ea3113bf33f99e7fa0c11207cf302f6a6d0f7b3
Christina Douglas <chrissyidouglas@gmail.com>
2019-07-26 10:37:02 -0700

    server setup

--- server.js
+++ server.js
@@ -1,3 +1,13 @@
 const express         = require('express');
 const bodyParser      = require('body-parser');
 const methodOverride  = require('method-override');
+
+require('./db/db');
+
+app.use(bodyParser.urlencoded({extended:false}));
+
+app.use(methodOverride('_method'));
+
+app.listen(3000, () => {
+    console.log('listening..... on port 3000');
+  });
