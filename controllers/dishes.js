e84f246d2e5f5e81511b7ff47c061b980f891db5
Christina Douglas <chrissyidouglas@gmail.com>
2019-07-26 13:29:46 -0700

    dishes index added, linked to home page

--- controllers/dishes.js
+++ controllers/dishes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const Dish = require('../models/Dish');
-const Restaurant = require('../model/Restaurant')
+// const Restaurant = require('../model/Restaurant')
 
 // index
 router.get('/', async (req, res)=>{
@@ -19,21 +19,21 @@ router.get('/', async (req, res)=>{
   });
 
 // new
-router.get('/new', (req, res)=>{
+// router.get('/new', (req, res)=>{
     
-    Restaurant.find({}, (err, allRestaurants) => {
-      if(err){
-        res.send(err);
-      } else {
-        console.log(allRestaurants, "< -- new route in dishes ")
-        console.log('restaurants array ^^^^^^^^^^^^^');
-        res.render('dishes/new.ejs', {
-          restaurants: allRestaurants
-        });
+//     Restaurant.find({}, (err, allRestaurants) => {
+//       if(err){
+//         res.send(err);
+//       } else {
+//         console.log(allRestaurants, "< -- new route in dishes ")
+//         console.log('restaurants array ^^^^^^^^^^^^^');
+//         res.render('dishes/new.ejs', {
+//           restaurants: allRestaurants
+//         });
   
-      }
-    })
+//       }
+//     })
   
-  });
+//   });
 
   module.exports = router
