a43f50de56d15b12fab0396d6e98d259742d4e8e
Christina Douglas <chrissyidouglas@gmail.com>
2019-07-26 10:47:38 -0700

    app added to server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express         = require('express');
 const bodyParser      = require('body-parser');
 const methodOverride  = require('method-override');
+const app             = express();
 
 require('./db/db');
 
