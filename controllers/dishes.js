a52debce5f632932effc523c08495b0e3988e177
Christina Douglas <chrissyidouglas@gmail.com>
2019-07-26 13:39:40 -0700

    restaurants index route created

--- controllers/dishes.js
+++ controllers/dishes.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const Dish = require('../models/Dish');
-// const Restaurant = require('../model/Restaurant')
+const Restaurant = require('../models/Restaurant')
 
 // index
 router.get('/', async (req, res)=>{
-    console.log(req.session, 'req.session in index or article')
+    console.log(req.session, 'req.session in index or dish')
     try {
       
       const foundDishes = await Dish.find();
@@ -18,22 +18,22 @@ router.get('/', async (req, res)=>{
     }
   });
 
-// new
-// router.get('/new', (req, res)=>{
+//new
+router.get('/new', (req, res)=>{
     
-//     Restaurant.find({}, (err, allRestaurants) => {
-//       if(err){
-//         res.send(err);
-//       } else {
-//         console.log(allRestaurants, "< -- new route in dishes ")
-//         console.log('restaurants array ^^^^^^^^^^^^^');
-//         res.render('dishes/new.ejs', {
-//           restaurants: allRestaurants
-//         });
+    Restaurant.find({}, (err, allRestaurants) => {
+      if(err){
+        res.send(err);
+      } else {
+        console.log(allRestaurants, "< -- new route in dishes ")
+        console.log('restaurants array ^^^^^^^^^^^^^');
+        res.render('dishes/new.ejs', {
+          restaurants: allRestaurants
+        });
   
-//       }
-//     })
+      }
+    })
   
-//   });
+  });
 
   module.exports = router
